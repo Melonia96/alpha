a52284c2c18c5df5d353039e3a57ff13d96bf9d0
Melonia96 <108812802+Melonia96@users.noreply.github.com>
2022-07-15 18:28:51 -0400

    Update AlphaApplication.java
    Copied in sample code

--- src/main/java/com/example/Aub/alpha/AlphaApplication.java
+++ src/main/java/com/example/Aub/alpha/AlphaApplication.java
@@ -11,3 +11,53 @@ public class AlphaApplication {
 	}
 
 }
+
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private final String name;
+    private final String email;
+    
+    // standard constructors / setters / getters / toString
+}
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+    // standard constructors
+    
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+}
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
